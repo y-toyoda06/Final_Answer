eb10c789ace33f48af68b070a38cac58a3c26483
tysl <tysl0111222@gmail.com>
2025-05-24 11:48:19 +0900

    課題追加

---
+++ Exercise_for_Pool/python/ex1_web-scraping/1-1.py
@@ -0,0 +1,126 @@
+import random
+from bs4 import BeautifulSoup
+import requests
+import time
+import re
+import pandas as pd
+
+# ユーザーエージェントの設定
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 14.5; rv:126.0) Gecko/20100101 Firefox/126.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0"
+]
+random_user_agent = random.choice(user_agents)
+headers = {
+    'user-agent': random_user_agent
+}
+
+SLEEP_TIME = 3
+MAX_RECORDS = 50
+
+def get_url(url):
+    """指定URLから1ページ分のリンクを抽出する"""
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    elems = soup.find_all("a", class_="style_titleLink___TtTO")
+    links = [elem.attrs['href'] for elem in elems if '?' not in elem.attrs['href']]
+    return links
+
+
+
+def collect_urls(base_url, max_records):
+    """ページをまたいでmax_records件までリンクを収集"""
+    page = 1
+    urls = []
+
+    while len(urls) < max_records:
+        url = base_url if page == 1 else f"{base_url}?p={page}"
+        print(f"ページ {page} を取得中...")
+        links = get_url(url)
+
+        if not links:
+            print("これ以上リンクが見つかりません．")
+            break
+
+        urls.extend(links)
+        page += 1
+        time.sleep(SLEEP_TIME)
+
+    return urls[:max_records]
+
+
+
+def get_store_info(url):
+    time.sleep(SLEEP_TIME)
+    response = requests.get(url, headers=headers)
+    response.encoding = response.apparent_encoding
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    #店舗名
+    name_tag = soup.find("p", id="info-name")
+    name = name_tag.get_text(strip=True) if name_tag else ""
+    
+    #電話番号
+    number_tag = soup.find("span", class_="number")
+    number = number_tag.get_text(strip=True) if number_tag else ""
+    
+    #メールアドレス
+    #email_tag = soup.find()
+    #email = email_tag.get_text(strip=True) if number_tag else ""
+    email = ""
+    
+    #住所
+    address_tag = soup.find("span", class_="region" )
+    address = address_tag.get_text(strip=True) if address_tag else ""
+    
+    building_tag = soup.find("span", class_="locality" )
+    building = building_tag.get_text(strip=True) if building_tag else ""
+    
+    #住所分割
+    pref, city, addr = split_address(address)
+    
+    #ulrは空白でsslはfalse(仕様)
+    url = ""
+    ssl = False    
+    
+    return{
+        "店舗名": name,
+        "電話番号": number,
+        "メールアドレス": email,
+        "都道府県": pref,
+        "市区町村": city,
+        "番地": addr,
+        "建物名": building,
+        "URL": url,
+        "SSL": ssl
+    }
+    
+# 住所を分割
+def split_address(address):
+    pattern = r"^(東京都|北海道|(?:京都|大阪)府|.{2,3}県)([^0-9]+)([\d\-]+)"
+    match = re.match(pattern, address)
+    if match:
+        return match.group(1), match.group(2), match.group(3) or ""
+    else:
+        return "", "", ""
+    
+    
+
+def main():
+    base_url = "https://r.gnavi.co.jp/area/tokyo/izakaya/rs/"
+    urls = collect_urls(base_url, MAX_RECORDS)
+    store_info = []
+
+    for u in urls:
+        print(u)
+    for url in urls:
+        store_info.append(get_store_info(url))
+        print(f"done: {url}")
+        
+        
+    df = pd.DataFrame(store_info,columns=["店舗名","電話番号","メールアドレス","都道府県","市区町村","番地","建物名","URL","SSL"])
+    df.to_csv("1-1.csv", index=False, encoding="utf-8-sig")
+
+if __name__ == "__main__":
+    main()
---
+++ Exercise_for_Pool/python/ex1_web-scraping/1-2.py
@@ -0,0 +1,150 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+import re
+
+
+
+def get_store_urls(driver):
+    # 一覧ページから店舗リンク取得
+    links = driver.find_elements(By.CSS_SELECTOR, "a.style_titleLink___TtTO")
+    return [link.get_attribute("href") for link in links]
+
+
+    ...
+
+def parse_store_info(driver, url):
+    driver.get(url)
+    time.sleep(3)
+
+    #店名
+    try:
+        name = driver.find_element(By.ID, "info-name").text
+    except:
+        name = ""
+        
+    #電話番号
+    try:
+        number = driver.find_element(By.CLASS_NAME, "number").text
+    except:
+        number = ""
+    
+    #住所
+    try:
+        address = driver.find_element(By.CLASS_NAME, "region").text
+    except:
+        address = ""
+    
+    
+    #建物名
+    try:
+        building = driver.find_element(By.CLASS_NAME, "locality").text
+    except:
+        building = ""
+        
+    pref, city, addr = split_address(address)
+    
+        
+    # お店のホームページをクリックしてURL取得
+    # お店のホームページURLとSSL
+    try:
+        link = driver.find_element(By.CSS_SELECTOR, ".url.go-off")
+        homepage = link.get_attribute("href")
+        ssl = homepage.startswith("https://")
+    except:
+        homepage = ""
+        ssl = "False"
+
+        
+    email =""
+        
+    return{
+        "店舗名": name,
+        "電話番号": number,
+        "メールアドレス": email,
+        "都道府県": pref,
+        "市区町村": city,
+        "番地": addr,
+        "建物名": building,
+        "URL": homepage,
+        "SSL": ssl
+    }
+        
+        
+# 住所を分割
+def split_address(address):
+    pattern = r"^(東京都|北海道|(?:京都|大阪)府|.{2,3}県)([^0-9]+)([\d\-]+)"
+    match = re.match(pattern, address)
+    if match:
+        return match.group(1), match.group(2), match.group(3) or ""
+    else:
+        return "", "", ""
+
+
+
+
+    
+def main():
+    base_url = "https://r.gnavi.co.jp/area/tokyo/izakaya/rs/"
+    driver = webdriver.Chrome()
+    driver.get(base_url)
+    time.sleep(3)
+
+    collected_data = []
+    page_count = 1  # ページカウンタ
+
+    while len(collected_data) < 50:
+        print(f"\nページ {page_count} 開始 - URL: {driver.current_url}")
+
+        # ストアURLの収集
+        urls = get_store_urls(driver)
+        print(f" 店舗URL取得：{len(urls)}件")
+
+
+        for url in urls:
+            if len(collected_data) >= 50:
+                break
+
+            store_info = parse_store_info(driver, url)
+            print(store_info)
+            
+            if len(collected_data) >= 50:
+                break
+            collected_data.append(store_info)
+            print(f"done: {store_info['店舗名']}")
+
+        # 「次へ」ボタンがあれば押す
+        try:
+            driver.get(base_url)
+            time.sleep(3)
+            next_btn = driver.find_element(By.XPATH, '//a[./img[contains(@alt, "次")]]')
+            current_url = driver.current_url
+            next_btn.click()
+            time.sleep(3)
+            base_url = driver.current_url
+            
+            if current_url == driver.current_url:
+                print("ページ遷移失敗")
+                break
+            else:
+                print(f"遷移：{current_url} → {base_url}")
+                page_count += 1
+
+        except:
+            print("次のページが見つかりません")
+            break
+
+    print(f"総レコード数: {len(collected_data)} 件")
+     # CSVに保存
+    df = pd.DataFrame(collected_data,columns=["店舗名","電話番号","メールアドレス","都道府県","市区町村","番地","建物名","URL","SSL"])
+    df.to_csv("1-2.csv", index=False, encoding="utf-8-sig")
+
+    driver.quit()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
